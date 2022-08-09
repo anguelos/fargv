01350a68f3cb6d80816792071c849b80fa4c111d
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2022-08-09 13:31:06 +0200

    tolerating non existing filename from invocker

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension
 
 setup(
     name='fargv',
-    version='0.1.6',
+    version='0.1.7',
     packages=['fargv'],
     license='MIT',
     author='Anguelos Nicolaou',
